1eda46f7656f339abde85fe9cc16b1fb32e06462
Thawatchai Sangthep <94940261+MickBerryZ@users.noreply.github.com>
2023-03-08 14:38:24 +0700

    Add project files.

---
+++ MvcCoreTutorial2/Controllers/PersonController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreTutorial2.Models.Domain;
+
+namespace MvcCoreTutorial2.Controllers
+{
+    public class PersonController : Controller
+    {
+        private readonly DatabaseContext _ctx;
+            
+        public PersonController(DatabaseContext ctx) 
+        {
+            _ctx = ctx;
+        }
+        public IActionResult Index()
+        {
+            ViewBag.greeting = "Hello world";
+            ViewData["greeting2"] = "I am using ViewData";
+            //ViewBag and ViewData can send data only from ControllerToView
+
+            //Tempdata can send data from ome controller method to another controller
+            TempData["greeting3"] = "It's temData msg";
+            return View();
+        }
+
+        // IT is get method
+        public IActionResult AddPerson()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddPerson(Person person)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View();
+            }
+            try
+            {
+                _ctx.Add(person);
+                _ctx.SaveChanges();
+                TempData["msg"] = "Added successfully";
+                return RedirectToAction("AddPerson");
+
+            }
+            catch (Exception ex)
+            {
+                TempData["msg"] = "Could not added!!!";
+                return View();
+            }
+        }
+    }
+}
---
+++ MvcCoreTutorial2/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using MvcCoreTutorial2.Models.Domain;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<DatabaseContext>(Options => Options.UseSqlServer(builder.Configuration.GetConnectionString("conn")));
+// whatever services are needed in application will be declared here
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+// section for configuring middlewares
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+// Index method of home controller will be execute first thing in the application
+// it is default URL
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Person}/{action=AddPerson}/{id?}");
+
+app.Run();
